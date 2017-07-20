9c2d6e4db48b9a00d0452e49ac2c1f1cf7ae4da9
Alex Zavgorodnev <azavgorodnev93@mail.ru>
2017-07-20 11:10:50 +0000

    capistrano

--- Gemfile
+++ Gemfile
@@ -5,3 +5,10 @@ gem 'rack'
 gem 'puma'
 gem 'pg'
 gem 'pry'
+
+group :deploy do
+  gem 'capistrano', '~> 3.2', :require => false
+  gem 'capistrano3-puma', require: false
+  gem 'capistrano-rbenv', :require => false
+  gem 'capistrano-bundler', :github => 'capistrano/bundler', :require => false
+end
---
+++ config/deploy.rb
@@ -0,0 +1,49 @@
+# config valid only for current version of Capistrano
+lock "3.8.2"
+
+set :application, "onelec_efind_rack"
+set :repo_url, "git@github.com:BrandyMint/onelec_efind_rack.git"
+
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, "/home/wwwonelec/#{fetch(:application)}"
+
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# append :linked_files, "config/database.yml", "config/secrets.yml"
+
+# Default value for linked_dirs is []
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets"
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for local_user is ENV['USER']
+# set :local_user, -> { `git config user.name`.chomp }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+set :rbenv_type, :user
+set :rbenv_ruby, File.read('.ruby-version').strip
+
+set :bundle_without, %w{development test deploy}.join(' ')
+set :bundle_jobs, 10
+
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true
+
+set :nginx_sites_available_path, "/etc/nginx/sites-available"
+set :nginx_sites_enabled_path, "/etc/nginx/sites-enabled"
